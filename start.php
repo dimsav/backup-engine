0515a397a35d602128245dba116242f52d431666
Dimitrios Savvopoulos <ds@dimsav.com>
2013-08-02 01:44:58 +0200

    Set the maximum execution time to unlimited.

--- start.php
+++ start.php
@@ -3,18 +3,20 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+set_time_limit ( 0 ); // Disable time limit
+
 define('DS', DIRECTORY_SEPARATOR);
 define('APP_PATH', dirname(__FILE__));
 
 define('VENDORS', APP_PATH . DS . 'vendors');
 define('LOGS_PATH', APP_PATH . DS . 'logs');
 
-require_once(APP_PATH . DS . 'config.php');
-require_once(VENDORS  . DS . 'Utilities.php');
-require_once(VENDORS  . DS . 'DropboxUploader' . DS . 'DropboxUploader.php');
-require_once(VENDORS  . DS . 'UnixZipper.php');
-require_once(VENDORS  . DS . 'klogger' . DS . 'klogger.class.php');
-require_once(VENDORS  . DS . 'MysqlBackup' . DS . 'MysqlBackup.php');
+require_once(APP_PATH . "/config.php");
+require_once(VENDORS  . "/Utilities.php");
+require_once(VENDORS  . "/DropboxUploader/DropboxUploader.php");
+require_once(VENDORS  . "/UnixZipper.php");
+require_once(VENDORS  . "/klogger/klogger.class.php");
+require_once(VENDORS  . "/MysqlBackup/MysqlBackup.php");
 
 $log = KLogger::instance(LOGS_PATH, false);
 $log->echo = true;
