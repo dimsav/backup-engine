bc589fadc0e33947b20b8515c5d18ec55f1b0073
Dimitrios Savvopoulos <ds@dimsav.com>
2013-09-14 00:25:00 +0200

    Created configuration class.

---
+++ src/Dimsav/PhpMysqlBackup/Config.php
@@ -0,0 +1,33 @@
+<?php namespace Dimsav\PhpMysqlBackup;
+
+use Illuminate\Config\FileLoader;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Config\Repository;
+
+
+class Config {
+
+    private static $instance;
+
+    public static function get($key, $default = null)
+    {
+        if ( ! self::$instance )
+        {
+            self::validate();
+            $fileLoader = new FileLoader(new Filesystem, __DIR__.'/../../..');
+            self::$instance = new Repository($fileLoader, 'config');
+        }
+        return self::$instance->get($key, $default);
+    }
+
+    private static function validate()
+    {
+        if ( ! is_dir(__DIR__.'/../../../config'))
+        {
+            throw new \Exception("The configuration directory is missing.".
+                "\nPlease copy the directory config.ini as config ".
+                "and customize the files inside according your preferences.");
+        }
+    }
+}
+
