b7944077e872fb48627a16cb3946df07005a6c62
Dimitrios Savvopoulos <ds@dimsav.com>
2013-09-14 18:24:02 +0200

    Updated the configuration structure.

--- config/projects.php
+++ config/projects.php
@@ -1,25 +1,16 @@
 <?php
 
-/*
- * Define here the projects you wish to backup.
- *
- * The key of the array marks the name of the project
- * and it is used for folder and file names. So better
- * use alphanumeric characters with slash/underscores.
- */
-
 return array(
 
     /*
-     * The "defaults" key is the only exception as
-     * it is not a project. It is used to define the
-     * default configuration for each project.
+     * The "default" key is used to define the
+     * default configuration values of each project.
      *
-     * If you define a password here, all the backups
-     * will be compressed using this password.
+     * For example, if you define a password here,
+     * all the backups will be compressed using this password.
      */
 
-    "defaults" => array(
+    "default" => array(
         "database" => array(
             "host" => "localhost",
             "port" => "3306",
@@ -30,80 +21,80 @@ return array(
     ),
 
     /*
-     * Here we define a project to be backed-up.
-     * For this project, we want to backup only
-     * the database. We use the default host and
-     * port, and we override the username and password.
+     * Define in this array the projects you wish to backup.
      *
-     * For this project we are overriding the default
-     * password with another one.
-     */
-    "project-1" => array(
-
-        "database" => array(
-            "database"=>"db-name",
-            "username"=>"db-user",
-            "password"=>"db-pass",
-        ),
-
-        "password" => "another-secret",
-    ),
-
-    /*
-     * For this project we backup both some files
-     * and the database.
-     *
-     * We use the default database settings, so we
-     * define only the database name.
-     *
-     * Under "paths" we put a list of absolute paths
-     * of directories or files.
-     *
-     * Under "excludes" we put a list of absolute paths
-     * of directories or files that should not be
-     * included in the compressed backup files. The
-     * contents of these directories will be skipped
-     * recursively.
-     */
-    "project-2" => array(
-
-        "database" => array(
-            "database"=>"db-name",
-        ),
-
-        "paths" => array(
-            "/absolute/project/folder/path",
-            "/absolute/project/file/text.txt",
-        ),
-
-        "excludes" => array(
-            "/absolute/project/folder/path/cache",
-            "/absolute/project/folder/path/logs",
-            "/absolute/project/folder/path/bigfile.tar",
-        ),
-    ),
-
-    /*
-     * Most of the times, projects have just one backup directory.
-     * In this case you can use the key "path" and set a string with
-     * the absolute path of the directory/file.
-     *
-     * Same goes for excludes. Use "exclude" and set a string.
-     *
-     * Note here that there is no strict type needed for paths and
-     * excludes. Both keys "paths" and "path" work with string and
-     * array types. Just make sure you don't set both keys in the
-     * same project. The same applies for "exclude" and "excludes"
+     * The key of the array marks the name of the project
+     * and it is used for folder and file names. So better
+     * use alphanumeric characters with slash/underscores.
      */
 
-    "project-3" => array(
+    "projects" => array(
 
-        "database" => array(
-            "database"=>"database-name",
+        /*
+         * Here we define a project to be backed-up.
+         * For this project, we want to backup only
+         * the database. We use the default host and
+         * port, and we override the username and password.
+         *
+         * For this project we are overriding the default
+         * password with another one.
+         */
+        "project-1" => array(
+
+            "database" => array(
+                "name"    =>"db-name",
+                "username"=>"db-user",
+                "password"=>"db-pass",
+            ),
+
+            "password" => "another-secret",
         ),
 
-        "path" => "/absolute/project/folder/path",
-        "exclude" => "/absolute/project/folder/path/cache",
+        /*
+         * For this project we backup both some files
+         * and the database.
+         *
+         * We use the default database settings, so we
+         * define only the database name.
+         *
+         * Under "paths" we put a list of absolute paths
+         * of directories or files.
+         *
+         * Under "excludes" we put a list of absolute paths
+         * of directories or files that should not be
+         * included in the compressed backup files. The
+         * contents of these directories will be skipped
+         * recursively.
+         */
+        "project-2" => array(
+
+            "database" => array(
+                "name"=>"db-name",
+            ),
+
+            "paths" => array(
+                "/absolute/project/folder/path",
+                "/absolute/project/file/text.txt",
+            ),
+
+            "excludes" => array(
+                "/absolute/project/folder/path/cache",
+                "/absolute/project/folder/path/logs",
+                "/absolute/project/folder/path/bigfile.tar",
+            ),
+        ),
+
+        /*
+         * Here we disable for project-3 the default password,
+         * as we don't want any password for this project.
+         */
+        "project-3" => array(
+            "paths" => array(
+                "/project/folder",
+            ),
+            "password" => null,
+        )
+
     ),
 
 );
