bf64f01e16b19d64500272a15492d378820ae278
Dimitrios Savvopoulos <ds@dimsav.com>
2013-09-15 14:45:42 +0200

    Moved showing of errors to higher position.

--- run.php
+++ run.php
@@ -3,6 +3,13 @@
 require_once(__DIR__.'/vendor/autoload.php');
 
 use Dimsav\Backup\Application;
+use Dimsav\Backup\Config;
+
+if (Config::get('app.debug', true))
+{
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+}
 
 $app = new Application();
 
--- src/Dimsav/Backup/Application.php
+++ src/Dimsav/Backup/Application.php
@@ -31,12 +31,6 @@ class Application {
 
     private function setup()
     {
-        if (Config::get('app.debug', true))
-        {
-            error_reporting(E_ALL);
-            ini_set('display_errors', '1');
-        }
-
         date_default_timezone_set(Config::get('app.timezone', 'Europe/Berlin'));
 
         set_time_limit (Config::get('app.time_limit', 0));
