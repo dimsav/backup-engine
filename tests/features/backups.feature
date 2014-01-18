1009f78a284c717f50d29e1f781bbca5b8efff96
Dimitrios Savvopoulos <ds@dimsav.com>
2014-01-19 00:18:08 +0100

    Added behat basic files.

---
+++ tests/features/backups.feature
@@ -0,0 +1,4 @@
+Feature: Backups on local system
+  In order to backup my projects
+  As an administrator
+  I need to save my projects in zipped archives locally
---
+++ tests/features/bootstrap/FeatureContext.php
@@ -0,0 +1,23 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+require_once(__DIR__.'/../../../vendor/autoload.php');
+
+/**
+ * Features context.
+ */
+class FeatureContext extends BehatContext
+{
+
+    public function __construct(array $parameters)
+    {
+
+    }
+
+}
