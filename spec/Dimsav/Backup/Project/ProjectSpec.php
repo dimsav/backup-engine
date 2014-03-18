45e321c49bde687b6cde0ee05d29dea4d1f1e3c8
Dimitrios Savvopoulos <ds@dimsav.com>
2014-03-19 00:53:05 +0100

    Added contents to new project class

--- spec/Dimsav/Backup/Project/ProjectSpec.php
+++ spec/Dimsav/Backup/Project/ProjectSpec.php
@@ -2,13 +2,40 @@
 
 namespace spec\Dimsav\Backup\Project;
 
+use Dimsav\Backup\Project\Database;
+use Dimsav\Backup\Project\Location;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class ProjectSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $this->beConstructedWith('projectName');
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Dimsav\Backup\Project\Project');
+        $this->getName()->shouldReturn('projectName');
     }
+
+    function it_receives_and_returns_databases(Database $database)
+    {
+        $this->addDatabase($database);
+        $this->getDatabases()->shouldReturn(array($database));
+    }
+
+    function it_receives_and_reeturns_paths(Location $path)
+    {
+        $this->addPath($path);
+        $this->getPaths()->shouldReturn(array($path));
+    }
+
+    function it_receives_and_returns_excludes(Location $exclude)
+    {
+        $this->addExclude($exclude);
+        $this->getExcludes()->shouldReturn(array($exclude));
+    }
+
 }
--- src/Dimsav/Backup/Project/Project.php
+++ src/Dimsav/Backup/Project/Project.php
@@ -1,5 +1,74 @@
 <?php namespace Dimsav\Backup\Project;
 
+use Illuminate\Support\Collection;
+
 class Project {
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var Database[]
+     */
+    private $databases = array();
+
+    /**
+     * @var Location[]
+     */
+    private $paths = array();
+
+    /**
+     * @var Location[]
+     */
+    private $excludes = array();
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function addDatabase(Database $database)
+    {
+        $this->databases[]= $database;
+    }
+
+    public function getDatabases()
+    {
+        return $this->databases;
+    }
+
+    public function addPath($path)
+    {
+        $this->paths[] = $path;
+    }
+
+    /**
+     * @return \Dimsav\Backup\Project\Location[]
+     */
+    public function getPaths()
+    {
+        return $this->paths;
+    }
+
+    public function addExclude(Location $exclude)
+    {
+        $this->excludes[] = $exclude;
+    }
+
+    public function getExcludes()
+    {
+        return $this->excludes;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
 
 }
