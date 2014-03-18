93f223320a481e3ff718e706c1289fac09075c14
Dimitrios Savvopoulos <ds@dimsav.com>
2014-03-19 00:14:10 +0100

    Added database fill method + test

--- spec/Dimsav/Backup/Project/DatabaseSpec.php
+++ spec/Dimsav/Backup/Project/DatabaseSpec.php
@@ -11,4 +11,24 @@ class DatabaseSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Dimsav\Backup\Project\Database');
     }
+
+    function it_should_be_fillable()
+    {
+        $data = array(
+            'name' => 'a',
+            'host' => 'b',
+            'port' => 'c',
+            'username' => 'd',
+            'password' => 'e',
+        );
+        $this->fill($data);
+
+        $this->getName()->shouldBe('a');
+        $this->getHost()->shouldBe('b');
+        $this->getPort()->shouldBe('c');
+        $this->getUsername()->shouldBe('d');
+        $this->getPassword()->shouldBe('e');
+    }
+
+
 }
--- src/Dimsav/Backup/Project/Database.php
+++ src/Dimsav/Backup/Project/Database.php
@@ -2,4 +2,59 @@
 
 class Database {
 
-} 
+    private $name;
+    private $host;
+    private $port;
+    private $username;
+    private $password;
+
+    public function fill($data)
+    {
+        $this->name = isset($data['name']) ? $data['name'] : $this->name;
+        $this->host = isset($data['host']) ? $data['host'] : $this->host;
+        $this->port = isset($data['port']) ? $data['port'] : $this->port;
+        $this->username = isset($data['username']) ? $data['username'] : $this->username;
+        $this->password = isset($data['password']) ? $data['password'] : $this->password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+}
