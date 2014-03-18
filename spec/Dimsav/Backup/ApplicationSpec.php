f6ea8830693cb9c8ec4fca531abb108bc6b32597
Dimitrios Savvopoulos <ds@dimsav.com>
2014-03-18 23:38:23 +0100

    Added phpspec as dev dependency

---
+++ spec/Dimsav/Backup/ApplicationSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\Dimsav\Backup;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ApplicationSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Dimsav\Backup\Application');
+    }
+}
