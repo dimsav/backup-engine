f4b8880c7276b5b39d477e27e513265e04bce9f6
Dimitrios Savvopoulos <ds@dimsav.com>
2014-03-19 00:33:44 +0100

    Added location test

--- spec/Dimsav/Backup/Project/LocationSpec.php
+++ spec/Dimsav/Backup/Project/LocationSpec.php
@@ -9,6 +9,15 @@ class LocationSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
+        $this->beConstructedWith('/');
         $this->shouldHaveType('Dimsav\Backup\Project\Location');
     }
+
+    function it_normalizes_paths()
+    {
+        $this->beConstructedWith(__DIR__.'/../Project');
+        $this->get()->shouldReturn(__DIR__);
+    }
+
+
 }
--- src/Dimsav/Backup/Project/Location.php
+++ src/Dimsav/Backup/Project/Location.php
@@ -4,4 +4,16 @@ namespace Dimsav\Backup\Project;
 
 class Location
 {
+
+    protected $path;
+
+    function __construct($path)
+    {
+        $this->path = realpath($path);
+    }
+
+    public function get()
+    {
+        return $this->path;
+    }
 }
