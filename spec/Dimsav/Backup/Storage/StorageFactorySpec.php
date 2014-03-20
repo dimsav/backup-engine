cc36554034a1eafdf5f0b1a99c85f9791e23102a
Dimitrios Savvopoulos <ds@dimsav.com>
2014-03-20 16:56:03 +0100

    Added StorageFactory

---
+++ spec/Dimsav/Backup/Storage/StorageFactorySpec.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace spec\Dimsav\Backup\Storage;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Illuminate\Container\Container;
+
+class StorageFactorySpec extends ObjectBehavior
+{
+    function let(Container $container)
+    {
+        $this->beConstructedWith($container);
+    }
+
+    function it_creates_accepts_dropbox_as_driver()
+    {
+        $config = array('driver' => 'dropbox');
+        $this->make($config)->shouldReturnAnInstanceOf('Dimsav\Backup\Storage\DropboxStorage');
+    }
+
+    function it_creates_accepts_file_local_as_driver()
+    {
+        $config = array('driver' => 'file_local');
+        $this->make($config)->shouldReturnAnInstanceOf('Dimsav\Backup\Storage\LocalFileStorage');
+    }
+
+    function it_throws_exception_if_driver_is_unknown_or_empty()
+    {
+        $config = array('driver' => 'abc');
+        $this->shouldThrow(new \InvalidArgumentException("Invalid storage driver"))->duringMake($config);
+    }
+
+
+}
---
+++ src/Dimsav/Backup/Storage/StorageFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Dimsav\Backup\Storage;
+
+use Illuminate\Container\Container;
+
+class StorageFactory
+{
+
+    /**
+     * @var Container
+     */
+    private $container;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    public function make($config, $name = null)
+    {
+        return $this->createStorage($config['driver'], $config);
+    }
+
+    private function createStorage($driver, $config)
+    {
+        switch ($driver)
+        {
+            case 'dropbox':
+                return new DropboxStorage($config);
+            case 'file_local':
+                return new LocalFileStorage($config);
+        }
+        throw new \InvalidArgumentException('Invalid storage driver');
+    }
+}
