3247c8ac63aa97850bae0d60373429cdce952b32
Dimitrios Savvopoulos <ds@dimsav.com>
2014-05-06 00:00:08 +0200

    Added storage validator class.

---
+++ spec/Dimsav/Backup/Validator/StorageValidatorSpec.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace spec\Dimsav\Backup\Validator;
+
+use Dimsav\Backup\Storage\Exceptions\TokenNotSetException;
+use Dimsav\Backup\Storage\Storage;
+use Dimsav\Backup\Storage\StorageFactory;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class StorageValidatorSpec extends ObjectBehavior
+{
+
+    function it_returns_a_string_if_storages_are_not_valid(StorageFactory $storageFactory, Storage $storage_1, Storage $storage_2)
+    {
+        $exception1 = new TokenNotSetException('Omg, what have you done??');
+        $exception2 = new TokenNotSetException('Forget about it. Go play some left 4 dead 2.');
+        $storageFactory->makeAll()->willReturn(array($storage_1, $storage_2));
+        $storage_1->validate()->shouldBeCalled()->willThrow($exception1);
+        $storage_2->validate()->shouldBeCalled()->willThrow($exception2);
+
+        $this->beConstructedWith($storageFactory);
+        $this->validate();
+        $this->getValidationErrorsString()->shouldBe("Error 1: Omg, what have you done??\nError 2: Forget about it. Go play some left 4 dead 2.");
+    }
+
+    function it_returns_false_if_storages_are_valid(StorageFactory $storageFactory, Storage $storage_1, Storage $storage_2)
+    {
+        $storageFactory->makeAll()->willReturn(array($storage_1, $storage_2));
+
+        $this->beConstructedWith($storageFactory);
+        $this->validate();
+        $this->getValidationErrorsString()->shouldReturn(false);
+    }
+
+}
---
+++ src/Dimsav/Backup/Validator/StorageValidator.php
@@ -0,0 +1,57 @@
+<?php namespace Dimsav\Backup\Validator;
+
+use Dimsav\Backup\Storage\Exceptions\InvalidStorageException;
+use Dimsav\Backup\Storage\Exceptions\TokenNotSetException;
+use Dimsav\Backup\Storage\StorageFactory;
+
+class StorageValidator
+{
+
+    /**
+     * @var \Dimsav\Backup\Storage\StorageFactory
+     */
+    private $storageFactory;
+
+    private $errors = array();
+
+    public function __construct(StorageFactory $storageFactory)
+    {
+        $this->storageFactory = $storageFactory;
+    }
+
+    public function validate()
+    {
+        foreach ($this->storageFactory->makeAll() as $storage)
+        {
+            try {
+                $storage->validate();
+            }
+            catch (TokenNotSetException $e) {
+                $this->errors[] = $e->getMessage();
+            }
+            catch (InvalidStorageException $e) {
+                $this->errors[] = $e->getMessage();
+            }
+        }
+    }
+
+
+    /**
+     * Returns false if no errors where found, else, a string with the error messages.
+     *
+     * @return false|string
+     */
+    public function getValidationErrorsString()
+    {
+        foreach ($this->errors as $key => $error)
+        {
+            $count = $key+1;
+            $this->errors[$key] = "Error $count: " . $error;
+        }
+        if ($this->errors)
+        {
+            return implode("\n", $this->errors);
+        }
+        return false;
+    }
+}
